78d8eaf31b68b2515d9cad7f97407714d16f8660
Nikita Lu <nik17042001@outlook.com>
2021-06-28 04:58:10 +0300

    lite-completed post request

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . '/layouts/header.php');
 ?>
     <h1>Archi Vision - сайт, который поможет вам определить стиль архитектурного сооружения по фото!</h1>
-
+    <form action="/post.php" method="post">
+        <label>Введите URL Изображения: <input type="text" name="URL" value=""/></label>
+        <button type="submit" name="send">Сканировать</button>
+    </form>
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/layouts/footer.php');
--- post.php
+++ post.php
@@ -1,8 +1,11 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/layouts/header.php');
+
+$strURL = htmlspecialchars($_POST["URL"]);
 
 $url = 'https://definingstylearchitecture.cognitiveservices.azure.com/customvision/v3.0/Prediction/947e6fce-51fd-4e92-9702-f9e36108f0ac/classify/iterations/Iteration7/url';
 
-$data = '{Url:"https://lh3.googleusercontent.com/proxy/gRh1pf2XVVk5EwzGA8a2h9I9nXPpFd59bkynpBCXyv42YkAXsXMbuH6d0RHna98xj9Rox1RqhFedhuayIpOjPRSYas1B6mJQHfVWhjlRABdwTT7L83xgfkflQfk62AjpyiX77g8"}';
+$data = '{Url:"' . $strURL . '"}';
 
 $options = array(
     'http' => array(
@@ -15,8 +18,17 @@ $options = array(
 $context  = stream_context_create($options);
 $result = file_get_contents( $url, false, $context );
 $response = json_decode($result, true);
-echo "<pre>" . print_r($response) . "</pre>";
-$answer0 = $response['predictions'][0]['tagName'] . "   " . $response['predictions'][0]['probability'] . "<br>";
-$answer1 = $response['predictions'][1]['tagName'] . "   " . $response['predictions'][1]['probability'] . "<br>";
-$answer2 = $response['predictions'][2]['tagName'] . "   " . $response['predictions'][2]['probability'] . "<br>";
-echo $answer0 . $answer1 . $answer2;
+
+if($response === NULL) {
+    echo "<br>Неверный URL изображения";
+}
+else {
+    //print_r($response);
+    $answer0 = $response['predictions'][0]['tagName'] . " с вероятностью " . $response['predictions'][0]['probability'] * 100 . " %" . "<br>";
+    $answer1 = $response['predictions'][1]['tagName'] . " с вероятностью " . $response['predictions'][1]['probability'] * 100 . " %" . "<br>";
+    $answer2 = $response['predictions'][2]['tagName'] . " с вероятностью " . $response['predictions'][2]['probability'] * 100 . " %" . "<br>";
+    echo "Архитектурные стили:<br><br>" . $answer0 . "<br>" . $answer1 . "<br>" . $answer2;
+}
+
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/layouts/footer.php');
